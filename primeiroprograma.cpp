a361c951a895e2d85d95021ba0efd30f14bc3e3c
Rebeca Lopes <rebeca.lopes.141@gmail.com>
2021-03-06 19:50:04 -0300

    Primeiro programa em c++

---
+++ primeiroprograma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+string nome;
+cout << "qual seu nome?" << endl;
+cin >> nome;
+cout << "muito prazer " + nome << endl;
+    return 0;
+}

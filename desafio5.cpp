579c6f08d5dafb7996b701f3d86348f2ca0c6f44
Rebeca Lopes <rebeca.lopes.141@gmail.com>
2021-04-03 17:19:32 -0300

    verificando requisitos para programa de moradia

---
+++ desafio5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+bool verificaRequisitos (int idade, float salario) {
+
+    if (idade == 21 && salario <= 1200) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main () {
+
+    int idade;
+    float salario;
+
+    cout << "Informe sua idade: " << endl;
+    cin >> idade;
+
+    cout << "Informe sua renda mensal: " << endl;
+    cin >> salario;
+
+    bool requisitos = verificaRequisitos(idade, salario);
+
+    if (requisitos == true) {
+        cout << "Parabens, voce se encaixa nos requisitos do programa Moradia Popular";
+    } else {
+        cout << "Infelizmente voce nao se encaixa nos requisitos do programa Moradia Popular";
+    }
+
+    return 0;
+}

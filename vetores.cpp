12b1f7dcc06abd3ce9b9ba69c5cf94e5d62000c6
Rebeca Lopes <rebeca.lopes.141@gmail.com>
2021-04-04 21:26:40 -0300

    alocação dinamica de vetores

--- vetores.cpp
+++ vetores.cpp
@@ -11,7 +11,7 @@ int main() {
     vet[1] = 4;
     vet[2] = 6;
 
-    int vetor[7] = {1,3};
+    int vetorzinho[7] = {1,3};
 
     int vet2[] = {3, 6, 9, 12, 15};
 
@@ -22,12 +22,28 @@ int main() {
     }
 
     for (int i = 0; i<7; i++){
-        cout << vetor[i] << endl;
+        cout << vetorzinho[i] << endl;
     }
 
     //verifica tamanho do vetor em bytes
-    int x = sizeof(vetor);
+    int x = sizeof(vetorzinho);
     cout << x << endl;
+
+    //alocação dinamica
+    int tamanho;
+    cout << "Digite o tamanho do vetor: " << endl;
+    cin >> tamanho;
+
+    int* vetor = new int[tamanho];
+
+    for (int i = 0; i < tamanho; i++){
+        cout << "Digite o elemento " << i+1 << " do vetor: " << endl;
+        cin >> vetor[i];
+    }
+
+    for (int i = 0; i < tamanho; i++){
+        cout << vetor[i] << " ";
+    }
 
     return 0;
 }
